58aa0a67b36583253bb792e95a02d0b13b3d18c0
rhinobucks <51710832+rhinobucks@users.noreply.github.com>
2019-08-12 13:58:56 -0500

    V1.0
    Specific Settings can be selected and executed. Missing Delay between each iteration.

---
+++ Set1_2.m
@@ -0,0 +1,749 @@
+clear all
+close all
+clc
+format long g
+
+%% User Input Block: (move to the top of the file after completion)
+
+test_name='Initial Program Test 1';
+target_size='2.4384m x 2.4384m';
+distance=1; %Distance to target in meters
+temperature= 74; %Degrees i n Farenheit 
+humidity= 45; %Humidity percentage
+ambient_light= 300; %Ambient light in lx
+
+target_notes='Notes Here'; %Reflectivity or other notes here 
+
+%%%%%%%%% Setting Entry %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+itn=14; %Number of iterations of grabbing data from sensor 
+
+TLx=0; %Default 0
+TLy=15; %Default 15
+BRx=15; %Default 15
+BRy=0; %Default 0
+
+set_char1='B';
+set_char2='B';
+set_char3='B';
+set_char4='B';
+set_char5='B';
+set_char6='B';
+set_char7='B';
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+space1=[' ',' '];
+space2=[' ',' ';' ',' '];
+colnum=10; 
+
+
+% Sets proper format of string to be sent to the microcontroller
+
+if TLx>=0 &&  TLx<=9
+ ch_TLx=strcat('0',int2str(TLx)); 
+ 
+else
+    ch_TLx=int2str(TLx);
+    
+end
+
+if TLy>=0 &&  TLy<=9
+ ch_TLy=strcat('0',int2str(TLy)); 
+ 
+else
+    ch_TLy=int2str(TLy);
+      
+end
+
+if BRx>=0 &&  BRx<=9
+ ch_BRx=strcat('0',int2str(BRx)); 
+   
+else
+    ch_BRx=int2str(BRx);
+    
+end
+
+if BRy>=0 &&  BRy<=9
+ ch_BRy=strcat('0',int2str(BRy)); 
+
+else
+    ch_BRy=int2str(BRy);
+       
+end
+
+if itn>=0 &&  itn<=9
+ ch_itn=strcat('0','0','0',int2str(itn)); 
+
+elseif itn>=10 &&  itn<=99
+    ch_itn=strcat('0','0',int2str(itn)); 
+        
+elseif itn>=100 &&  itn<=999
+     ch_itn=strcat('0',int2str(itn)); 
+     
+else
+    ch_itn=int2str(itn); 
+       
+end
+
+
+
+
+
+
+%% Iteration 1 %%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%
+itn_num=1;
+
+
+%%%%%%%%%%%%%%%%
+i=1; 
+itn_matrix=zeros(itn,1); 
+
+while(i<itn+1)
+    
+    itn_matrix(i,1)=itn_num; 
+    i=i+1;
+   
+end
+
+
+set1=strcat(set_char1,ch_TLx,ch_TLy,ch_BRx,ch_BRy, ch_itn);
+i=1;
+
+cur_tim=' ';
+
+
+data_array = zeros(itn, colnum-1);
+
+sObject=serial('COM6','BaudRate',115200,'TimeOut',10,'Terminator','LF');
+
+
+get(sObject);
+fopen(sObject);
+
+fprintf(sObject,set1); 
+sObject.ValuesSent %Confirms data being sent 
+
+
+prev_t=0;
+new_t=0;
+tim=zeros(1,itn);
+T=tic; %stopwatch starts 
+
+
+while(i<itn+1)
+    
+    fprintf(sObject,'*IDN?');
+    scan = fscanf(sObject);
+    
+    tim(i)=toc(T);
+    if i==1
+       prev_t=0;
+        
+    else
+       prev_t=tim(i-1);   
+    end
+    
+    new_t=tim(i)-prev_t; 
+    
+    cur_tim=num2str(new_t,8);
+      
+    out=strcat(scan,',',cur_tim);
+    data_array(i,:)=str2num(out); 
+    prev_t=new_t;
+    i=i+1;
+    
+end
+
+
+
+fclose(sObject);
+fprintf('Port Closed!\n'); 
+
+
+
+end_tim=num2str(toc(T),8);
+avg_tim=num2str((toc(T)/itn),8);
+
+data_array;
+D1=[data_array itn_matrix];
+
+
+%% Iteration 2 %%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%
+itn_num=2;
+
+
+%%%%%%%%%%%%%%%%
+i=1; 
+itn_matrix=zeros(itn,1); 
+
+while(i<itn+1)
+    
+    itn_matrix(i,1)=itn_num; 
+    i=i+1;
+   
+end
+
+
+set1=strcat(set_char2,ch_TLx,ch_TLy,ch_BRx,ch_BRy, ch_itn);
+i=1;
+
+cur_tim=' ';
+
+
+data_array = zeros(itn, colnum-1);
+
+sObject=serial('COM6','BaudRate',115200,'TimeOut',10,'Terminator','LF');
+
+
+get(sObject);
+fopen(sObject);
+
+fprintf(sObject,set1); 
+sObject.ValuesSent %Confirms data being sent 
+
+
+prev_t=0;
+new_t=0;
+tim=zeros(1,itn);
+T=tic; %stopwatch starts 
+
+
+while(i<itn+1)
+    
+    fprintf(sObject,'*IDN?');
+    scan = fscanf(sObject);
+    
+    tim(i)=toc(T);
+    if i==1
+       prev_t=0;
+        
+    else
+       prev_t=tim(i-1);   
+    end
+    
+    new_t=tim(i)-prev_t; 
+    
+    cur_tim=num2str(new_t,8);
+      
+    out=strcat(scan,',',cur_tim);
+    data_array(i,:)=str2num(out); 
+    prev_t=new_t;
+    i=i+1;
+    
+end
+
+
+
+fclose(sObject);
+fprintf('Port Closed!\n'); 
+
+
+
+end_tim=num2str(toc(T),8);
+avg_tim=num2str((toc(T)/itn),8);
+
+data_array;
+D2=[data_array itn_matrix];
+
+
+
+%% Iteration 3 %%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%
+itn_num=3;
+
+
+%%%%%%%%%%%%%%%%
+i=1; 
+itn_matrix=zeros(itn,1); 
+
+while(i<itn+1)
+    
+    itn_matrix(i,1)=itn_num; 
+    i=i+1;
+   
+end
+
+
+set1=strcat(set_char3,ch_TLx,ch_TLy,ch_BRx,ch_BRy, ch_itn);
+i=1;
+
+cur_tim=' ';
+
+
+data_array = zeros(itn, colnum-1);
+
+sObject=serial('COM6','BaudRate',115200,'TimeOut',10,'Terminator','LF');
+
+
+get(sObject);
+fopen(sObject);
+
+fprintf(sObject,set1); 
+sObject.ValuesSent %Confirms data being sent 
+
+
+prev_t=0;
+new_t=0;
+tim=zeros(1,itn);
+T=tic; %stopwatch starts 
+
+
+while(i<itn+1)
+    
+    fprintf(sObject,'*IDN?');
+    scan = fscanf(sObject);
+    
+    tim(i)=toc(T);
+    if i==1
+       prev_t=0;
+        
+    else
+       prev_t=tim(i-1);   
+    end
+    
+    new_t=tim(i)-prev_t; 
+    
+    cur_tim=num2str(new_t,8);
+      
+    out=strcat(scan,',',cur_tim);
+    data_array(i,:)=str2num(out); 
+    prev_t=new_t;
+    i=i+1;
+    
+end
+
+
+
+fclose(sObject);
+fprintf('Port Closed!\n'); 
+
+
+
+end_tim=num2str(toc(T),8);
+avg_tim=num2str((toc(T)/itn),8);
+
+data_array;
+D3=[data_array itn_matrix];
+
+
+%% Iteration 4 %%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%
+itn_num=4;
+
+
+%%%%%%%%%%%%%%%%
+i=1; 
+itn_matrix=zeros(itn,1); 
+
+while(i<itn+1)
+    
+    itn_matrix(i,1)=itn_num; 
+    i=i+1;
+   
+end
+
+
+set1=strcat(set_char3,ch_TLx,ch_TLy,ch_BRx,ch_BRy, ch_itn);
+i=1;
+
+cur_tim=' ';
+
+
+data_array = zeros(itn, colnum-1);
+
+sObject=serial('COM6','BaudRate',115200,'TimeOut',10,'Terminator','LF');
+
+
+get(sObject);
+fopen(sObject);
+
+fprintf(sObject,set1); 
+sObject.ValuesSent %Confirms data being sent 
+
+
+prev_t=0;
+new_t=0;
+tim=zeros(1,itn);
+T=tic; %stopwatch starts 
+
+
+while(i<itn+1)
+    
+    fprintf(sObject,'*IDN?');
+    scan = fscanf(sObject);
+    
+    tim(i)=toc(T);
+    if i==1
+       prev_t=0;
+        
+    else
+       prev_t=tim(i-1);   
+    end
+    
+    new_t=tim(i)-prev_t; 
+    
+    cur_tim=num2str(new_t,8);
+      
+    out=strcat(scan,',',cur_tim);
+    data_array(i,:)=str2num(out); 
+    prev_t=new_t;
+    i=i+1;
+    
+end
+
+
+
+fclose(sObject);
+fprintf('Port Closed!\n'); 
+
+
+
+end_tim=num2str(toc(T),8);
+avg_tim=num2str((toc(T)/itn),8);
+
+data_array;
+D4=[data_array itn_matrix];
+
+
+
+%% Iteration 5 %%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%
+itn_num=5;
+
+
+
+%%%%%%%%%%%%%%%%
+i=1; 
+itn_matrix=zeros(itn,1); 
+
+while(i<itn+1)
+    
+    itn_matrix(i,1)=itn_num; 
+    i=i+1;
+   
+end
+
+
+set1=strcat(set_char5,ch_TLx,ch_TLy,ch_BRx,ch_BRy, ch_itn);
+i=1;
+
+cur_tim=' ';
+
+
+data_array = zeros(itn, colnum-1);
+
+sObject=serial('COM6','BaudRate',115200,'TimeOut',10,'Terminator','LF');
+
+
+get(sObject);
+fopen(sObject);
+
+fprintf(sObject,set1); 
+sObject.ValuesSent %Confirms data being sent 
+
+
+prev_t=0;
+new_t=0;
+tim=zeros(1,itn);
+T=tic; %stopwatch starts 
+
+
+while(i<itn+1)
+    
+    fprintf(sObject,'*IDN?');
+    scan = fscanf(sObject);
+    
+    tim(i)=toc(T);
+    if i==1
+       prev_t=0;
+        
+    else
+       prev_t=tim(i-1);   
+    end
+    
+    new_t=tim(i)-prev_t; 
+    
+    cur_tim=num2str(new_t,8);
+      
+    out=strcat(scan,',',cur_tim);
+    data_array(i,:)=str2num(out); 
+    prev_t=new_t;
+    i=i+1;
+    
+end
+
+
+
+fclose(sObject);
+fprintf('Port Closed!\n'); 
+
+
+
+end_tim=num2str(toc(T),8);
+avg_tim=num2str((toc(T)/itn),8);
+
+data_array;
+D5=[data_array itn_matrix];
+
+
+%% Iteration 6 %%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%
+itn_num=6;
+
+
+
+%%%%%%%%%%%%%%%%
+i=1; 
+itn_matrix=zeros(itn,1); 
+
+while(i<itn+1)
+    
+    itn_matrix(i,1)=itn_num; 
+    i=i+1;
+   
+end
+
+
+set1=strcat(set_char6,ch_TLx,ch_TLy,ch_BRx,ch_BRy, ch_itn);
+i=1;
+
+cur_tim=' ';
+
+
+data_array = zeros(itn, colnum-1);
+
+sObject=serial('COM6','BaudRate',115200,'TimeOut',10,'Terminator','LF');
+
+
+get(sObject);
+fopen(sObject);
+
+fprintf(sObject,set1); 
+sObject.ValuesSent %Confirms data being sent 
+
+
+prev_t=0;
+new_t=0;
+tim=zeros(1,itn);
+T=tic; %stopwatch starts 
+
+
+while(i<itn+1)
+    
+    fprintf(sObject,'*IDN?');
+    scan = fscanf(sObject);
+    
+    tim(i)=toc(T);
+    if i==1
+       prev_t=0;
+        
+    else
+       prev_t=tim(i-1);   
+    end
+    
+    new_t=tim(i)-prev_t; 
+    
+    cur_tim=num2str(new_t,8);
+      
+    out=strcat(scan,',',cur_tim);
+    data_array(i,:)=str2num(out); 
+    prev_t=new_t;
+    i=i+1;
+    
+end
+
+
+
+fclose(sObject);
+fprintf('Port Closed!\n'); 
+
+
+
+end_tim=num2str(toc(T),8);
+avg_tim=num2str((toc(T)/itn),8);
+
+data_array;
+D6=[data_array itn_matrix];
+
+
+%% Iteration 7 %%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%
+itn_num=7;
+
+
+
+%%%%%%%%%%%%%%%%
+i=1; 
+itn_matrix=zeros(itn,1); 
+
+while(i<itn+1)
+    
+    itn_matrix(i,1)=itn_num; 
+    i=i+1;
+   
+end
+
+
+set1=strcat(set_char7,ch_TLx,ch_TLy,ch_BRx,ch_BRy, ch_itn);
+i=1;
+
+cur_tim=' ';
+
+
+data_array = zeros(itn, colnum-1);
+
+sObject=serial('COM6','BaudRate',115200,'TimeOut',10,'Terminator','LF');
+
+
+get(sObject);
+fopen(sObject);
+
+fprintf(sObject,set1); 
+sObject.ValuesSent %Confirms data being sent 
+
+
+prev_t=0;
+new_t=0;
+tim=zeros(1,itn);
+T=tic; %stopwatch starts 
+
+
+while(i<itn+1)
+    
+    fprintf(sObject,'*IDN?');
+    scan = fscanf(sObject);
+    
+    tim(i)=toc(T);
+    if i==1
+       prev_t=0;
+        
+    else
+       prev_t=tim(i-1);   
+    end
+    
+    new_t=tim(i)-prev_t; 
+    
+    cur_tim=num2str(new_t,8);
+      
+    out=strcat(scan,',',cur_tim);
+    data_array(i,:)=str2num(out); 
+    prev_t=new_t;
+    i=i+1;
+    
+end
+
+
+
+fclose(sObject);
+fprintf('Port Closed!\n'); 
+
+
+
+end_tim=num2str(toc(T),8);
+avg_tim=num2str((toc(T)/itn),8);
+
+data_array;
+D7=[data_array itn_matrix];
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% Header Printing Portion: Text File Header Setup
+
+DateString = datestr(datetime);
+
+
+file_data = fopen('SensorData.txt','w');
+fprintf(file_data, 'Sensor Data Acquisition\n');
+fclose(file_data);
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+
+file_data = fopen('SensorData.txt','a');
+fprintf(file_data, '%s\n\n', DateString);
+fclose(file_data);
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+
+file_data = fopen('SensorData.txt','a');
+fprintf(file_data, '%s',test_name);
+fclose(file_data);
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+
+file_data = fopen('SensorData.txt','a');
+fprintf(file_data, 'Distance to Target: %dm',distance);
+fclose(file_data);
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+
+file_data = fopen('SensorData.txt','a');
+fprintf(file_data, 'Target Size: %s ', target_size);
+fclose(file_data);
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+
+file_data = fopen('SensorData.txt','a');
+fprintf(file_data, 'Environment Information: Temp %dF, Humidity %d%% , Ambient Light %d lux', temperature, humidity, ambient_light);
+fclose(file_data);
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+
+file_data = fopen('SensorData.txt','a');
+fprintf(file_data, 'Average Time Elapsed: %s sec', avg_tim);
+fclose(file_data);
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+
+file_data = fopen('SensorData.txt','a');
+fprintf(file_data, 'Number of Samples Taken: %d samples', itn);
+fclose(file_data);
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+
+file_data = fopen('SensorData.txt','a');
+fprintf(file_data, 'Total Time Elapsed: %s sec', end_tim);
+fclose(file_data);
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+
+file_data = fopen('SensorData.txt','a');
+fprintf(file_data, '%s',target_notes);
+fclose(file_data);
+
+
+
+%% Data Printing Section:
+dlmwrite('SensorData.txt',space2,'delimiter',' ','newline', 'pc','-append')
+dlmwrite('SensorData.txt',D1,'delimiter',',','newline', 'pc','-append')
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+dlmwrite('SensorData.txt',D2,'delimiter',',','newline', 'pc','-append')
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+dlmwrite('SensorData.txt',D3,'delimiter',',','newline', 'pc','-append')
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+dlmwrite('SensorData.txt',D4,'delimiter',',','newline', 'pc','-append')
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+dlmwrite('SensorData.txt',D5,'delimiter',',','newline', 'pc','-append')
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+dlmwrite('SensorData.txt',D6,'delimiter',',','newline', 'pc','-append')
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+dlmwrite('SensorData.txt',D7,'delimiter',',','newline', 'pc','-append')
+dlmwrite('SensorData.txt',space1,'delimiter',' ','newline', 'pc','-append')
+
+
+
+%Confimation of data acquisition:
+fprintf("Acquisition Complete\n")
+
+
+
